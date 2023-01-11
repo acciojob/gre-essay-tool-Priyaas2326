39515a63d5282856920b3364525908217fa00643
Priyaas2326 <113659062+Priyaas2326@users.noreply.github.com>
2023-01-11 20:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+let evaluatedText = document.getElementById('evaluatedText');
+let wordCount = document.getElementById('wordCount');
+let button = document.getElementById('button');
+
+
+function countWords(){
+    if(evaluatedText.value === ""){
+        console.log("0");
+        wordCount.innerText = 0;
+    }
+    else{
+
+        const arr = evaluatedText.value.trim().split(" ");
+        console.log(evaluatedText.value);
+        if(arr.length > 0){
+            console.log(evaluatedText.value.length);
+            console.log(arr.length);
+            wordCount.innerText = arr.length;
+        }
+    }
+}
